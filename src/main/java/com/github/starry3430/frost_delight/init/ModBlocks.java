7e3592f979fac609e8ce4dd7a7250ecf686babb4
Starry. <2069130069@qq.com>
2026-07-31 20:58:30 +0800

    26.7.31

--- src/main/java/com/github/starry3430/frost_delight/init/ModBlocks.java
+++ src/main/java/com/github/starry3430/frost_delight/init/ModBlocks.java
@@ -18,7 +18,7 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, FrostDelight.MOD_ID);
-
+    //注册方块
     public static final RegistryObject<Block> ICE_CREAM_MACHINE = BLOCKS.register("machine/ice_cream_machine", IceCreamMachine::new);
     public static final RegistryObject<Block> FREEZING_MACHINE = registerBlock("machine/freezing_machine",
             () -> new Block(BlockBehaviour.Properties.of().strength(2.5F).requiresCorrectToolForDrops()));
@@ -26,6 +26,8 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of().strength(2.5F).requiresCorrectToolForDrops()));
     public static final RegistryObject<Block> ICE_CREAM_SCOOP = registerBlock("food/ice_cream_scoop",
             () -> new Block(BlockBehaviour.Properties.of().strength(1.0F).requiresCorrectToolForDrops()));
+
+    //---------------------------------------------------------------------------------------------------------//
 
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
